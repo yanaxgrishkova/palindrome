904231f0c8180736eb26d3bb7ddb4603fdab2943
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-10-14 01:13:58 +0300

    Delete palindrome.hpp

--- include/palindrome.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PALINDROME_HPP
-#define PALINDROME_HPP
-
-#include <locale>
-#include <string>
-
-auto palindrome(std::wstring const & string, std::locale const & locale = std::locale()) -> bool;
-
-#endif 
